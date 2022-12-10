b78252aa3343a35f44a2694480afe00aa0209821
saba_guledani <saba.guledani.1@btu.edu.ge>
2022-12-10 14:46:04 +0400

    first commit

---
+++ app/src/main/java/com/example/firebase/LogInActivity.kt
@@ -0,0 +1,45 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import com.example.firebase.databinding.ActivityLogInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+
+class LogInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val email = binding.email
+        val password = binding.password
+        val btn = binding.createbtn
+        
+        btn.setOnClickListener { 
+            if (email.text.toString().isValidEmail() && password.text.toString() != "" 
+                && password.text.toString().length >= 6 ){
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email.text.toString(),password.text.toString())
+                    .addOnCompleteListener {task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "წარმატებით დარეგისტრირდით!", Toast.LENGTH_SHORT).show()
+                            
+                        }else{
+                            Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show()
+                        }
+                        
+                    }
+                
+            }
+        }
+
+    }
+
+    fun String.isValidEmail() =
+        !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
